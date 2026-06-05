51d6698c271f562d46d2fd58553c95317abb782d
blurmemory <joshue3345@gmail.com>
2026-06-05 11:51:44 -0400

    mod listas con ejercicios

--- listas.py
+++ listas.py
@@ -1,7 +1,7 @@
 # uso y explicacion de listas
 #      -5   -4  -3  -2 -1
 list = [91, -7, 44, 88, 4]
-#       0   1   2  3   4
+#       0   1   2   3   4
 
 # for i in list:
 #     print(i*2)
@@ -11,10 +11,101 @@ list = [91, -7, 44, 88, 4]
 # for p in pokemons:
 #     print(p.upper())
 
-fruit = ["Manzana", "Melon", "Tomate", "Banana", "Mango"]
+# fruit = ["Manzana", "Melon", "Tomate", "Banana", "Mango"]
 
-for f in fruit:
-    if f[-1].lower() == "a":
-        print(f"La fruta {f} termina con A")
+# for f in fruit:
+#     if f[-1].lower() == "a":
+#         print(f"La fruta {f} termina con A")
+#     else:
+#         print(f"La fruta {f} no termina con A")
+
+
+
+# list1 = ["Diego", "Adolfo", "Mayte"]
+# list2 = ["Robles", "Hinako", "Silva"]
+
+
+# no=input("Agregue un nombre: ")
+# ape=input("Agregue un apellido: ")
+
+
+# list1.append(no)
+# list2.append(ape)
+
+# for f in range(len(list1)):
+#     print(list1[f], list2[f])
+
+toys = ["yo-yo", "tetris"]
+
+def add():
+    nameT = input("Ingrese el nombre del juguete: ")
+    toys.append(nameT)
+    print(f"Se ha añadido el juguete: {nameT}")
+    print("-"*30)
+
+def upd():
+    show()
+    upd = int(input("¿Que juguete desea actualizar: "))
+    newT = input("Ingrese el nuevo juguete: ")
+    toys[upd-1]=newT
+    print("-"*30)
+
+def delete():
+    show()
+    delete = int(input("¿Que juguete desea eliminar?: "))
+    toys.pop(delete-1)
+    print("Juguete Eliminado.")
+
+def show():
+    count = 1
+    for t in toys:
+        print(count,".-",t)
+        count+=1
+    print("-"*30)
+
+def menuT():
+    while True:
+            try:
+                op=0
+                print("1.- Agregar Juguete")
+                print("2.- Eliminar Juguete")
+                print("3.- Actualizar Juguete")
+                print("4.- Mostrar Juguete")
+                print("5.- Salir")
+                op=int(input("Ingrese una opción: "))
+                match op:
+                    case 1:
+                        add()
+                    case 2:
+                        delete()
+                    case 3:
+                        show()
+                        upd()
+                    case 4:
+                        show()
+                    case 5:
+                        print("Saliendo...")
+                        break
+                    case _:
+                        print("Opción Inválida")
+            except Exception as e:
+                print("Error,", e)
+
+listN = []
+pares = []
+impares = []
+
+num = input("Ingrese números enteros separados por espacio: ")
+listnum = num.split()
+
+for p in listnum:
+    listN.append(int(p))
+
+for n in listN:
+    if n % 2 == 0:
+        pares.append(n)
     else:
-        print(f"La fruta {f} no termina con A")
+        impares.append(n)
+
+print(f"Los números pares son: {pares}")
+print(f"Los números impares son: {impares}")
